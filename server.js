880cd1a2b115d7a9a971657409c98a2dc36faed0
tardisdriver <stark.tracy@gmail.com>
2017-06-29 20:53:09 -0500

    got integration test working

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+
+
+function runServer() {
+  const port = process.env.PORT || 8888;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+//runServer();
+
+module.exports = {app, runServer, closeServer};
---
+++ test/integration_test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+//const mongoose = require('mongoose');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('GET endpoint', function() {
+	before(function() {
+    	return runServer();
+  		});
+
+	after(function() {
+    	return closeServer();
+ 		 });
+
+	it('should return HTML on GET', function() {
+		return chai.request(app)
+		.get('/')
+		.then(function(res) {
+			res.should.have.status(200);
+			res.should.be.html;
+		});
+	});
+});

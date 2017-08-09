c3d3b5527db65a0ef50185a973fbd7bdc145bef0
tardisdriver <stark.tracy@gmail.com>
2017-08-09 17:41:30 -0500

    changes to config to support mlab mongo

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
+    'mongodb://travel-user:Travel1290@ds151289.mlab.com:51289/travel-buddy' ||
     'mongodb://localhost/tripsData';
 
 exports.TEST_DATABASE_URL = 'mongodb://localhost/tripsTest';

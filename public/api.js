20d7ea2b9ce9a393ab0e85f1c6b1c7c8329ed6db
tardisdriver <stark.tracy@gmail.com>
2017-09-13 21:34:50 -0500

    restructured code into an Itinerary class

--- public/api.js
+++ public/api.js
@@ -2,93 +2,64 @@ const localObj = 'localObj';
 
 const retrieveFromLocal = () => {
     const data = localStorage.getItem(localObj);
-    const demoTrips = [
-        {
-            date: "2019/11/23",
-            destination: "Greece",
-            budget: 4000,
-            costs:
-            [
-                { name: "airfare", value: 1000 },
-                { name: "lodging", value: 900 }
-            ],
-
-        },
-        {
-            date: "2018/04/23",
-            destination: "Australia",
-            budget: 5000,
-            costs:
-            [
-                { name: "airfare", value: 1300 },
-                { name: "lodging", value: 1000 }
-            ],
-        }
-    ];
     if (data) {
         return JSON.parse(localStorage.getItem(localObj));
     } else {
-        return demoTrips;
+        return [];
     }
 }
 
-const saveToLocal = (obj) => {
-    console.log('saveToLocal ran');
-    localStorage.setItem('localObj', JSON.stringify(obj));
-}
-
-
-function saveTrips(trips) {
-    if (username) {
-        //save to API
-        $.ajax({
-            type: 'PUT',
-            url: `http://localhost:8080/itineraries/${username}`,
-            data: trips
+//if username specified, retrieve trips from server and save locally, otherwise just use local storage
+const getTrips = (user) => {
+    if (user) {
+        return $.ajax({
+            type: 'GET',
+            url: `http://localhost:8080/itineraries/${username}`
+        }).then(trips => {
+            var itinerary = new Itinerary(trips, user);
+            itinerary.saveToLocal();
+            return itinerary;
         });
     } else {
-        //save to Local
-        itemIndex = parseInt(itemIndex);
-        return getTrips()
-            .then((currentTrips) => {
-                currentTrips[itemIndex] = updatedTrip;
-                return saveTrips(currentTrips);
+        return Promise.resolve(new Itinerary(retrieveFromLocal()));
+    }
+}
+
+class Itinerary {
+    constructor(trips, username) {
+        this.trips = trips
+        this.username = username
+    }
+
+    addTrip(trip) {
+        this.trips.push(trip);
+        return this.saveTrips();
+    }
+
+    editTrip(updatedTrip, itemIndex) {
+        this.trips[itemIndex] = updatedTrip;
+        return this.saveTrips();
+    }
+
+    deleteTrip(itemIndex) {
+        this.trips.splice(itemIndex, 1);
+        return this.saveTrips();
+    }
+
+    saveTrips() {
+        this.saveToLocal();
+        if (this.username) {
+            //save to API
+            return $.ajax({
+                type: 'PUT',
+                url: `http://localhost:8080/itineraries/${this.username}`,
+                data: this.trips
             });
-    }
-}
-
-const addTrip = (obj) => {
-    return getTrips()
-        .then((currentTrips) => {
-            currentTrips.push(obj)
-            return save(currentTrips)
-        })
-}
-
-const getTrips = () => {
-    return Promise.resolve(retrieveFromLocal());
-}
-
-
-
-
-const editTrip = (updatedTrip, itemIndex) => {
-    //when they set username, take the whole Trips JSON and send to API put request  
-    //in state, save username, then call save,
-    //if there's a username, save to database, if not save to localstorage
-    function saveUsername() {
-        //grab username from script.js 
-        handleSetUsername();
+        }
+        return Promise.resolve();
     }
 
-
+    saveToLocal() {
+        localStorage.setItem('localObj', JSON.stringify(this.trips));
+    }
 }
-
-const deleteTrip = (itemIndex) => {
-    return getTrips()
-        .then((trips) => {
-            trips.splice(itemIndex, 1)
-            return saveTrips(trips)
-        })
-}
-

7f507dcc0ef2187a1752e686b27683f59e91ff6a
tardisdriver <stark.tracy@gmail.com>
2017-10-04 15:05:25 -0500

    user interface improvements

--- public/user-interface.js
+++ public/user-interface.js
@@ -13,3 +13,18 @@ $('.unhide-saved-trips').on('click', function () {
     $('#js-trip-list-form').toggleClass('hidden');
     $('.saved-trips').toggleClass('hidden');
 });
+
+$('.initial-save-btn').on('click', function () {
+    $('.initial').addClass('hidden');
+    $('.choice').removeClass('hidden');
+});
+
+$('.new-user-btn').on('click', function () {
+    $('.new-user').removeClass('hidden');
+    $('.choice').addClass('hidden');
+});
+
+$('.returning-user-btn').on('click', function () {
+    $('.returning-user').removeClass('hidden');
+    $('.choice').addClass('hidden');
+});

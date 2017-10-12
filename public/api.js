ce44bf63b935db8446928ab31a2b7886c486c6e4
tardisdriver <stark.tracy@gmail.com>
2017-10-12 17:47:49 -0500

    eslint code cleanup

--- public/api.js
+++ public/api.js
@@ -1,71 +1,46 @@
+/* global localStorage $ */
 const localObj = 'localObj';
 
 const retrieveFromLocal = () => {
-    const data = localStorage.getItem(localObj);
-    if (data) {
-        return JSON.parse(data);
-    } else {
-        return [];
-    }
+  const data = localStorage.getItem(localObj);
+  if (data) {
+    return JSON.parse(data);
+  }
+  return [];
+};
+
+function saveToLocal(trips) {
+  localStorage.setItem(localObj, JSON.stringify(trips));
 }
 
 function saveTrips(trips, username) {
-    saveToLocal(trips);
-    if (username) {
-        $.ajax({
-            type: 'PUT',
-            url: `/itineraries/${username}`,
-            data: JSON.stringify(trips),
-            contentType: 'application/json'
-        });
-    }
+  saveToLocal(trips);
+  if (username) {
+    $.ajax({
+      type: 'PUT',
+      url: `/itineraries/${username}`,
+      data: JSON.stringify(trips),
+      contentType: 'application/json',
+    });
+  }
 }
 
-function saveToLocal(trips) {
-    localStorage.setItem(localObj, JSON.stringify(trips));
-}
-
-//if username specified, retrieve trips from server and save locally, otherwise just use local storage
 const getTrips = (username) => {
-    if (username) {
-        const ajaxOptions = {
-            type: 'GET',
-            url: `/itineraries/${username}`
-        }
-        return $.ajax(ajaxOptions)
-            .then(res => res.trips)
-
-    } else {
-        return Promise.resolve(retrieveFromLocal());
-    }
-}
-
-//function that checks username in the DB
-//  do a .find(_username_) on DB
-//      do GET where username equals entry
+  if (username) {
+    const ajaxOptions = {
+      type: 'GET',
+      url: `/itineraries/${username}`,
+    };
+    return $.ajax(ajaxOptions)
+      .then(res => res.trips);
+  }
+  return Promise.resolve(retrieveFromLocal());
+};
 
 function checkUsername(username) {
-    console.log('checkUsername Ran');
-    return $.ajax({
-        type: 'GET',
-        url: `/itineraries/${username}`
-    })
-        .then(function (res) {
-            if (res.trips.length === 0) {
-                var exists = false;
-            } else {
-                exists = true;
-            }
-            console.log(exists);
-            return exists;
-        })
+  return $.ajax({
+    type: 'GET',
+    url: `/itineraries/${username}`,
+  })
+    .then(res => res.trips.length !== 0);
 }
-
-
-
-
-// on create user
-
-// if username exists, display error message
-
-

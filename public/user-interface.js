6d573a8e9b562184ab05df08d0a51614a4e90838
tardisdriver <stark.tracy@gmail.com>
2017-10-16 11:56:26 -0500

    ESLint cleanup

--- public/user-interface.js
+++ public/user-interface.js
@@ -1,31 +1,26 @@
+/* global $ */
 
-
-//move this to a different function that just looks at the username as a parameter
-//everytime there is a potential modify of the username, call that function after that w/ new username
-
-
-
-$('.unhide-add-trips').on('click', function () {
-    $('#js-trip-list-form').toggleClass('hidden');
-    $('.saved-trips').toggleClass('hidden');
+$('.unhide-add-trips').on('click', () => {
+  $('#js-trip-list-form').toggleClass('hidden');
+  $('.saved-trips').toggleClass('hidden');
 });
 
-$('.unhide-saved-trips').on('click', function () {
-    $('#js-trip-list-form').toggleClass('hidden');
-    $('.saved-trips').toggleClass('hidden');
+$('.unhide-saved-trips').on('click', () => {
+  $('#js-trip-list-form').toggleClass('hidden');
+  $('.saved-trips').toggleClass('hidden');
 });
 
-$('.initial-save-btn').on('click', function () {
-    $('.initial').addClass('hidden');
-    $('.choice').removeClass('hidden');
+$('.initial-save-btn').on('click', () => {
+  $('.initial').addClass('hidden');
+  $('.choice').removeClass('hidden');
 });
 
-$('.new-user-btn').on('click', function () {
-    $('.new-user').removeClass('hidden');
-    $('.choice').addClass('hidden');
+$('.new-user-btn').on('click', () => {
+  $('.new-user').removeClass('hidden');
+  $('.choice').addClass('hidden');
 });
 
-$('.returning-user-btn').on('click', function () {
-    $('.returning-user').removeClass('hidden');
-    $('.choice').addClass('hidden');
+$('.returning-user-btn').on('click', () => {
+  $('.returning-user').removeClass('hidden');
+  $('.choice').addClass('hidden');
 });

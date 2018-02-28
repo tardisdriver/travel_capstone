e36edd566f8c225ae25b2edd093e8f054fc4f47d
tardisdriver <stark.tracy@gmail.com>
2018-02-27 18:53:46 -0600

    added favicon

--- models.js
+++ models.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const tripSchema = mongoose.Schema({
-    date: { type: String },
-    destination: { type: String },
-    budget: { type: Number },
-    costs:
+  date: { type: String },
+  destination: { type: String },
+  budget: { type: Number },
+  costs:
     [
-        {
-            name: String,
-            value: Number
-        }
-    ]
+      {
+        name: String,
+        value: Number,
+      },
+    ],
 });
 
 const itinerarySchema = mongoose.Schema({
-    username: { type: String },
-    trips: [tripSchema]
+  username: { type: String },
+  trips: [tripSchema]
 });
 
 const Itinerary = mongoose.models.Itinerary || mongoose.model('Itinerary', itinerarySchema);
